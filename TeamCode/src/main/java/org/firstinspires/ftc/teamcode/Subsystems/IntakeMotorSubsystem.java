0cb0cf5a043653b3235349cb0943e4dfa74d792e
Mason <mason.fouty@icloud.com>
2023-11-01 18:55:21 -0700

    fixed bug for reversing the motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeMotorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeMotorSubsystem.java
@@ -22,8 +22,7 @@ public class IntakeMotorSubsystem extends SubsystemBase {
     /// it tells the intake motor to run in reverse
     public void reverse() {
         intakeMotor.setRunMode(Motor.RunMode.RawPower);
-        intakeMotor.set(Configuration.INTAKE_MOTOR_POWER);
-        intakeMotor.setInverted(true);
+        intakeMotor.set(-Configuration.INTAKE_MOTOR_POWER);
     }
     public void stop() {
         intakeMotor.stopMotor();
