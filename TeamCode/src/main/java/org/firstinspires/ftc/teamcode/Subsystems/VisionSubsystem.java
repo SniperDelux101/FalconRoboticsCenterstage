8694e4671e68f3b4a0df1e33fd919ba03a582b35
Mason <mason.fouty@icloud.com>
2023-11-09 16:53:29 -0800

    Created a new subsystem for vision made functions to stop, resume and close the camera streaming

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VisionSubsystem.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import android.util.Size;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Utilities.Configuration;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+public class VisionSubsystem {
+    private static  final String TFOD_MODEL_FILE = "";
+    private static final String[] LABELS ={
+            "TeamProp"
+    };
+    private final HardwareMap hardwareMap;
+
+    private TfodProcessor tfod;
+
+    ///the variable to store our
+    private VisionPortal visionPortal;
+
+    public VisionSubsystem (VisionPortal vp , HardwareMap hm ){
+        visionPortal = vp;
+        hardwareMap = hm;
+    }
+    public void initTfod(boolean allowStreaming ){
+        tfod = new TfodProcessor.Builder()
+                .setModelFileName(TFOD_MODEL_FILE)
+                .setModelLabels(LABELS)
+                .setIsModelTensorFlow2(true)
+                .setIsModelQuantized(true)
+                .setModelInputSize(300)
+                .setModelAspectRatio(16.0 / 9.0)
+                .build();
+
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcame 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.enableLiveView(allowStreaming);
+        if (allowStreaming ){
+            builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        }
+        builder.addProcessor(tfod);
+
+        visionPortal = builder.build();
+        tfod.setMinResultConfidence((float) Configuration.CONFIDENCE_SCORE);
+
+    }
+
+    public void enableTfod (boolean ennabled ){
+        ///TODO: Come back and do type checking fot null values
+        visionPortal.setProcessorEnabled(tfod, ennabled);
+    }
+    public void stopStreaming(){
+        visionPortal.stopStreaming();
+    }
+    public void resumeStreaming(){
+        visionPortal.resumeStreaming();
+    }
+    public void closeVisionPortal(){
+        visionPortal.close();
+    }
+
+
+}
+
