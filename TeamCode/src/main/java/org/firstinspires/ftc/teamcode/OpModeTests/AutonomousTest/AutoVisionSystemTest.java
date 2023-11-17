393c6e0716973498d23bf4bd780b924a3f2d833a
Mason <mason.fouty@icloud.com>
2023-11-16 17:40:46 -0800

    created a new test for the autonomous.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AutoVisionSystemTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.OpModeTests.AutonomousTest;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.TeamPropPosition;
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.VisionSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.drive.FalconMecanumDrive;
+
+@Autonomous
+public class AutoVisionSystemTest extends OpMode {
+    private VisionSubsystem visionSubsystem ;
+    public static TeamPropPosition teamPropPosition = TeamPropPosition.Center;
+
+    @Override
+    public void init() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+         visionSubsystem= new VisionSubsystem(hardwareMap);
+
+    }
+    @Override
+    public void loop() {
+
+    }
+
+}
