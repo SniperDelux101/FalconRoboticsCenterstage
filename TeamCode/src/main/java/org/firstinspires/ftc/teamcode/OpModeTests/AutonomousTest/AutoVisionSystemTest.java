d679c3d30e55ad4d269943ba40cf355195dc98e6
Mason <mason.fouty@icloud.com>
2023-11-17 16:15:38 -0800

    implemented code for vision test system, set the team prop position to right as defalut. added the x and y values of the team prop to the configuration.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AutoVisionSystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AutoVisionSystemTest.java
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.teamcode.OpModeTests.AutonomousTest;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.Commands.Autonomous.TeamPropPosition;
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.VisionSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.drive.FalconMecanumDrive;
+import org.firstinspires.ftc.teamcode.Utilities.Configuration;
 
-@Autonomous
+import java.util.List;
+
+@Config
+@TeleOp(group = "subsystems test")
 public class AutoVisionSystemTest extends OpMode {
     private VisionSubsystem visionSubsystem ;
     public static TeamPropPosition teamPropPosition = TeamPropPosition.Center;
@@ -25,11 +32,43 @@ public class AutoVisionSystemTest extends OpMode {
     @Override
     public void loop() {
         if (getRecognition) {
-            teamPropPosition = visionSubsystem.getTeamPropPosition();
-            getRecognition = false;
+            teamPropPosition = this.getTeamPropPosition();
+            getRecognition = true;
         }
         telemetry.addData("Team Prop Position : ", teamPropPosition);
         telemetry.update();
+    }
+    public TeamPropPosition getTeamPropPosition(){
+        List<Recognition> currentRecognitions = visionSubsystem.getRecognitions();
+        TeamPropPosition position = TeamPropPosition.Right;
+        Recognition teamProp = null;
+        if(currentRecognitions != null){
+            for (Recognition recognition : currentRecognitions){
+
+                if (teamProp == null){
+                    teamProp = recognition;
+                }
+                else if (recognition.getConfidence()> teamProp.getConfidence()){
+                    teamProp = recognition;
+                }
+            }
+
+            if (teamProp != null ) {
+                double x= (teamProp.getLeft()+ teamProp.getRight()) /2 ;
+                double y = (teamProp.getTop()+ teamProp.getBottom()) / 2;
+                telemetry.addData("X value :", x );
+                telemetry.addData("Y Value : ",y );
+                if (x< Configuration.LEFT_UPPER_BOUND){
+                    position = TeamPropPosition.Left;
+                }
+                else if  (x > Configuration.LEFT_UPPER_BOUND && x< Configuration.RIGHT_LOWER_BOUND){
+                    position = TeamPropPosition.Center;
+                }
+                else if ( x > Configuration.RIGHT_LOWER_BOUND)
+                    position = TeamPropPosition.Right;
+            }
+        }
+        return position;
     }
 
 }
