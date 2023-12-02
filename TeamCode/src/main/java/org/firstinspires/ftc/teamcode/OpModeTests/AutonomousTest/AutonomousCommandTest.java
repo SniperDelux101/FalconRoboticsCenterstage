3e244244fd0280ea96b5eb1727e6c8c5927cc49d
Madgenius1324 <teiganbronec1@gmail.com>
2023-12-01 21:47:14 -0800

    conditions for the Over all Autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AutonomousCommandTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AutonomousCommandTest.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.OpModeTests.AutonomousTest;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SelectCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,6 +17,10 @@ import org.firstinspires.ftc.teamcode.Commands.Autonomous.Alliance;
 import org.firstinspires.ftc.teamcode.Commands.Autonomous.AutonomousDriveCommand;
 import org.firstinspires.ftc.teamcode.Commands.Autonomous.AutonomousPaths;
 import org.firstinspires.ftc.teamcode.Commands.Autonomous.AutonomousStartLocation;
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths.BuildBlueFarPaths;
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths.BuildBlueNearPaths;
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths.BuildRedFarPaths;
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths.BuildRedNearPaths;
 import org.firstinspires.ftc.teamcode.Commands.Autonomous.TeamPropPosition;
 import org.firstinspires.ftc.teamcode.Commands.IntakeCommand;
 import org.firstinspires.ftc.teamcode.Commands.PlacePixelOnSpikeCommand;
@@ -29,8 +38,10 @@ import org.firstinspires.ftc.teamcode.Subsystems.drive.FalconMecanumDrive;
 import org.firstinspires.ftc.teamcode.Subsystems.drive.TrajectorySequence.TrajectorySequence;
 
 import java.security.PublicKey;
+import java.util.HashMap;
 
 @Autonomous
+@Config
 public class AutonomousCommandTest extends CommandOpMode {
     ///region Subsystems
     private MecanumDriveSubsystem driveBaseSubsystem;
@@ -40,14 +51,17 @@ public class AutonomousCommandTest extends CommandOpMode {
     private LinearSlideSubsystem linearSlideSubsystem;
     private OdometryControlSubsystem odometryControlSubsystem;
     private IntakeMotorSubsystem intakeMotorSubsystem;
-    ///endregion
     private VisionSubsystem visionSubsystem;
-    public static Alliance alliance = Alliance.Red;
+    ///endregion
+
+    public static Alliance alliance = Alliance.Blue;
     public static AutonomousStartLocation startLocation = AutonomousStartLocation.Near;
     public static TeamPropPosition teamPropPosition = TeamPropPosition.Center;
 
     @Override
     public void initialize() {
+        CommandScheduler.getInstance().reset();
+
         driveBaseSubsystem = new MecanumDriveSubsystem(new FalconMecanumDrive(hardwareMap), false);
         odometryControlSubsystem = new OdometryControlSubsystem(hardwareMap, telemetry);
         airplaneLauncherSubsystem = new AirplaneLauncherSubsystem(hardwareMap, telemetry);
@@ -55,27 +69,9 @@ public class AutonomousCommandTest extends CommandOpMode {
         extakeSubsystem = new ExtakeSubsystem(hardwareMap, telemetry);
         linearSlideSubsystem = new LinearSlideSubsystem(hardwareMap, telemetry);
         intakeMotorSubsystem = new IntakeMotorSubsystem(hardwareMap, telemetry);
-        //TODO: Fix the vision portal
         visionSubsystem = new VisionSubsystem(hardwareMap, telemetry);
 
-        telemetry.addLine("Before Schedule");
-        telemetry.update();
-
-        schedule(
-                new WaitUntilCommand(this::isStarted).andThen(
-                        new TrajectorySequenceFollowerCommand(driveBaseSubsystem, BlueNearCenter_Phase1()),
-                        // This places a pixel on the spike
-                        new PlacePixelOnSpikeCommand(intakeMotorSubsystem).withTimeout(2000),
-
-                        new TrajectorySequenceFollowerCommand(driveBaseSubsystem, BlueNearCenter_Phase2())
-                )
-        );
-//        schedule(
-//                new WaitUntilCommand( this::isStarted).andThen(
-//                        new TrajectoryFollowerCommand(driveBaseSubsystem, BlueNearCenter_Phase1()),
-//
-//                )
-//        );
+        visionSubsystem.initTfod(true);
 
         register(driveBaseSubsystem, airplaneLauncherSubsystem, climbSubsystem, extakeSubsystem, linearSlideSubsystem, odometryControlSubsystem, intakeMotorSubsystem, visionSubsystem);
 
@@ -83,6 +79,68 @@ public class AutonomousCommandTest extends CommandOpMode {
         telemetry.update();
     }
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+
+        while(this.opModeInInit()) {
+            if (gamepad1.dpad_left)
+                alliance = Alliance.Red;
+            else if (gamepad1.dpad_right)
+                alliance = Alliance.Blue;
+            else if (gamepad1.dpad_up)
+                startLocation = AutonomousStartLocation.Far;
+            else if (gamepad1.dpad_down)
+                startLocation = AutonomousStartLocation.Near;
+
+            telemetry.addData("Alliance: ", alliance);
+            telemetry.addData("Auto Start Location: ", startLocation);
+            telemetry.update();
+        }
+
+        waitForStart();
+        teamPropPosition = visionSubsystem.getTeamPropPosition();
+        telemetry.addData("Team Prop Position: ", teamPropPosition);
+        telemetry.update();
+        visionSubsystem.closeVisionPortal();
+
+        TrajectorySequence phase1, phase2;
+
+        if(startLocation == AutonomousStartLocation.Near && alliance == Alliance.Blue) {
+            BuildBlueNearPaths.Build(driveBaseSubsystem.getDrive(), teamPropPosition);
+            phase1 = BuildBlueNearPaths.Phase1;
+            phase2 = BuildBlueNearPaths.Phase2;
+        } else if(startLocation == AutonomousStartLocation.Far  && alliance == Alliance.Blue){
+            BuildBlueFarPaths.Build(driveBaseSubsystem.getDrive(), teamPropPosition);
+            phase1 = BuildBlueFarPaths.Phase1;
+            phase2 = BuildBlueFarPaths.Phase2;
+        }
+        else if(startLocation == AutonomousStartLocation.Near && alliance == Alliance.Red){
+            BuildRedNearPaths.Build(driveBaseSubsystem.getDrive(), teamPropPosition);
+            phase1 = BuildRedNearPaths.Phase1;
+            phase2 = BuildRedNearPaths.Phase2;
+        } else{
+            BuildRedFarPaths.Build(driveBaseSubsystem.getDrive(), teamPropPosition);
+            phase1 = BuildRedFarPaths.Phase1;
+            phase2 = BuildRedFarPaths.Phase2;
+        }
+
+        schedule(
+                    new SequentialCommandGroup(
+                    new TrajectorySequenceFollowerCommand(driveBaseSubsystem, phase1),
+                    // This places a pixel on the spike
+                    new PlacePixelOnSpikeCommand(intakeMotorSubsystem).withTimeout(2000),
+                    new TrajectorySequenceFollowerCommand(driveBaseSubsystem, phase2)
+                )
+        );
+
+        // run the scheduler
+        while (!isStopRequested() && opModeIsActive()) {
+            run();
+        }
+        reset();
+    }
+/*
     public TrajectorySequence BlueNearCenter_Phase1(){
        return driveBaseSubsystem.getDrive().trajectorySequenceBuilder(new Pose2d())
                .strafeRight(5)
@@ -272,6 +330,7 @@ public class AutonomousCommandTest extends CommandOpMode {
                 .forward(32.0)
                 .build();
     }
+ */
 
 }
 
