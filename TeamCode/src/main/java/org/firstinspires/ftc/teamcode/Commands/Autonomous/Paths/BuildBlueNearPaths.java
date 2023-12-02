43234f3f26680c49821ec78f29b0e803592c3ea9
Madgenius1324 <teiganbronec1@gmail.com>
2023-12-01 18:33:58 -0800

    Build separate classes for the four diffirent alliances Autonomous pathings

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Autonomous/Paths/BuildBlueNearPaths.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths;
+
+import android.print.PageRange;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate;
+
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.TeamPropPosition;
+import org.firstinspires.ftc.teamcode.Subsystems.drive.FalconMecanumDrive;
+import org.firstinspires.ftc.teamcode.Subsystems.drive.TrajectorySequence.TrajectorySequence;
+
+@Config
+public class BuildBlueNearPaths {
+    public static class Params {
+        public static double BlueNearCenter_Phase1Strafe = 5 ;
+        public static double BlueNearCenter_Phase1Forward= 32;
+        public static double BlueNearCenter_Phase2Back= 28;
+        public static double BlueNearCenter_Phase2Forward = 35;
+        // values for BlueNearLeft
+        public static double BlueNearLeft_Phase1Strafe = 9;
+        public static double BlueNearLeft_Phase1Forward = 28;
+        public static double BlueNearLeft_Phase2back = 23 ;
+        public static double BlueNearLeft_Phase2Forward = 29;
+        // values for the near right blue
+        public static double BlueNearRight_Phase1Forward = 29;
+        public static double BlueNearRight_Phase2Forward = 9 ;
+        public static double BlueNearRight_Phase2Back1= 5.0;
+        public static double BlueNearRight_Phase2StrafeRight = 25;
+        public static double BlueNearRight_Phase2Back2 = 35 ;
+
+    }
+    public static TrajectorySequence Phase1 , Phase2 ;
+    FalconMecanumDrive drive;
+    public void Build (FalconMecanumDrive drive , TeamPropPosition position){
+        this.drive = drive;
+        if ( position == TeamPropPosition.Center){
+            BlueNearCenter_Phase1();
+            BlueNearCenter_Phase2();
+        }
+        else if (position == TeamPropPosition.Left ){
+            BlueNearLeft_Phase1();
+            BlueNearLeft_Phase2();
+        }
+        else {
+            BlueNearRight_Phase1();
+            BlueNearRight_Phase2();
+        }
+    }
+
+    public void BlueNearCenter_Phase1(){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .strafeRight(Params.BlueNearCenter_Phase1Strafe)
+                .forward(Params.BlueNearCenter_Phase1Forward)
+                .build();
+    }
+    public void BlueNearCenter_Phase2(){
+        Phase2= drive.trajectorySequenceBuilder(new Pose2d())
+                .back(Params.BlueNearCenter_Phase2Back)
+                //.waitSeconds(1)
+                .turn(Math.toRadians(90) + 1e-6)
+               // .waitSeconds(1)
+                .forward(Params.BlueNearCenter_Phase2Forward)
+                .build();
+    }
+    public void BlueNearLeft_Phase1 (){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .strafeLeft(Params.BlueNearLeft_Phase1Strafe)
+                .forward(Params.BlueNearLeft_Phase1Forward)
+                .build();
+    }
+    public void BlueNearLeft_Phase2 (){
+        Phase2= drive.trajectorySequenceBuilder(new Pose2d())
+                .back(Params.BlueNearLeft_Phase2back)
+                .turn(Math.toRadians(90) + 1e-6)
+                .forward(Params.BlueNearLeft_Phase2Forward)
+                .build();
+    }
+    public void BlueNearRight_Phase1 (){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.BlueNearRight_Phase1Forward)
+                .turn(Math.toRadians(-90) - 1e-6)
+                .build();
+    }
+    public void BlueNearRight_Phase2 () {
+        Phase2 = drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.BlueNearRight_Phase2Forward)
+                .back(Params.BlueNearRight_Phase2Back1)
+                .strafeRight(Params.BlueNearRight_Phase2StrafeRight)
+                .back(Params.BlueNearRight_Phase2Back2)
+                .build();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Autonomous/Paths/BuildRedFarPaths.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.Commands.Autonomous.Paths;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+import org.firstinspires.ftc.teamcode.Commands.Autonomous.TeamPropPosition;
+import org.firstinspires.ftc.teamcode.Subsystems.drive.FalconMecanumDrive;
+import org.firstinspires.ftc.teamcode.Subsystems.drive.TrajectorySequence.TrajectorySequence;
+
+public class BuildRedFarPaths {
+    public static class Params {
+        public static double RedFarCenter_Phase1Forward= 50;
+        public static double RedFarCenter_Phase2Forward1 = 5;
+        public static double RedFarCenter_Phase2Back= 8;
+        public static double RedFarCenter_Phase2Forward2 = 90;
+
+        // values for RedNearLeft
+        public static double RedFarLeft_Phase1Forward = 29;
+        public static double RedFarLeft_Phase2Forward1 = 9;
+        public static double RedFarLeft_Phase2back = 8 ;
+        public static double RedFarLeft_Phase2StrafeRight = 25 ;
+        public static double RedFarLeft_Phase2Back2 = 93 ;
+
+        // values for the near right red
+        public static double RedFarRight_Phase1Forward = 38;
+        public static double RedFarRight_Phase2Forward1 = 9 ;
+        public static double RedFarRight_Phase2Back1= 8.0;
+        public static double RedFarRight_Phase2StrafeLeft = 25.0;
+        public static double RedFarRight_Phase2Forward2 = 93 ;
+
+    }
+    public static TrajectorySequence Phase1 , Phase2 ;
+    FalconMecanumDrive drive;
+    public void Build (FalconMecanumDrive drive , TeamPropPosition position){
+        this.drive = drive;
+        if ( position == TeamPropPosition.Center){
+            RedFarCenter_Phase1();
+            RedFarCenter_Phase2();
+        }
+        else if (position == TeamPropPosition.Left ){
+            RedFarLeft_Phase1();
+            RedFarLeft_Phase2();
+        }
+        else {
+            RedFarRight_Phase1();
+            RedFarRight_Phase2();
+        }
+    }
+    public void RedFarCenter_Phase1(){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.RedFarCenter_Phase1Forward)
+                .turn(Math.toRadians(180) + 1e-6)
+                .build();
+
+    }
+    public void RedFarCenter_Phase2(){
+        Phase2= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.RedFarCenter_Phase2Forward1)
+                .back(Params.RedFarCenter_Phase2Back)
+                .turn(Math.toRadians(90) + 1e-6)
+                .forward(Params.RedFarCenter_Phase2Forward2)
+                .build();
+
+    }
+    public void RedFarLeft_Phase1(){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.RedFarLeft_Phase1Forward)
+                .turn(Math.toRadians(90) + 1e-6)
+                .build();
+
+    }
+    public void RedFarLeft_Phase2(){
+        Phase2= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.RedFarLeft_Phase2Forward1)
+                .back(Params.RedFarLeft_Phase2back)
+                .strafeRight(Params.RedFarLeft_Phase2StrafeRight)
+                .back(Params.RedFarLeft_Phase2Back2)
+                .build();
+    }
+
+
+    public void RedFarRight_Phase1(){
+        Phase1= drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(Params.RedFarRight_Phase1Forward)
+                .turn(Math.toRadians(-90) - 1e-6)
+                .build();
+    }
+    public void RedFarRight_Phase2 (){
+        Phase2= drive.trajectorySequenceBuilder(new Pose2d())
+                .back(Params.RedFarRight_Phase2Back1)
+                .strafeLeft(Params.RedFarRight_Phase2StrafeLeft)
+                .forward(Params.RedFarRight_Phase2Forward2)
+                .build();
+    }
+}
