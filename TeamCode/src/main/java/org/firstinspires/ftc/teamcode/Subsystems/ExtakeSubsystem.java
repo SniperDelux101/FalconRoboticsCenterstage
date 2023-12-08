805357fd76ff449bc3feaed08a9a8a7e3d77066f
Madgenius1324 <teiganbronec1@gmail.com>
2023-12-07 17:37:23 -0800

    Added LED control for amount of pixels in extake box 0 pixels = RED 1 pixel = PARTY WAVE 2 pixels = GREEN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ExtakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ExtakeSubsystem.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Utilities.Configuration;
 
 public class ExtakeSubsystem extends FalconSubsystemBase {
@@ -17,6 +21,11 @@ public class ExtakeSubsystem extends FalconSubsystemBase {
     private final SimpleServo controlArm1;
     //private final  SimpleServo controlArm2;
 
+    private final RevColorSensorV3 colorSensor1;
+    private final RevColorSensorV3 colorSensor2;
+
+    RevBlinkinLedDriver blinkinLedDriver;
+
 
     public ExtakeSubsystem(HardwareMap hMap, Telemetry tel) {
         super(tel);
@@ -24,6 +33,29 @@ public class ExtakeSubsystem extends FalconSubsystemBase {
         pixelWheel = hMap.get(CRServo.class, "Pixel_Wheel");
         controlArm1 = new SimpleServo(hMap, "Arm1_Servo", 0, 255);   //Configuration.E_MIN_ARM_ANGLE, Configuration.E_MAX_ARM_ANGLE);
         //controlArm2 = new SimpleServo(hMap, "Arm2_Servo", 0, 255); //Configuration.E_MIN_ARM_ANGLE, Configuration.E_MAX_ARM_ANGLE);
+        colorSensor1 = hMap.get(RevColorSensorV3.class, "colorSensor1");
+        colorSensor2 = hMap.get(RevColorSensorV3.class, "colorSensor2");
+        blinkinLedDriver = hMap.get(RevBlinkinLedDriver.class, "blinkin");
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+    }
+
+    public void detectPixel () {
+        //if distance is greater than 20 MM no pixel, on average pixel was about 6-7 MM away
+        telemetry.addLine()
+                .addData("Distance sensor1 ", "%.3f", colorSensor1.getDistance(DistanceUnit.MM))
+                .addData("Sensor 2", "%.3f", colorSensor2.getDistance(DistanceUnit.MM));
+        double sensor1Distance = colorSensor1.getDistance(DistanceUnit.MM);
+        double sensor2Distance = colorSensor2.getDistance(DistanceUnit.MM);
+        double distanceMax = 20.0;
+        if (sensor1Distance > distanceMax && sensor2Distance > distanceMax) {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+        } else if (sensor1Distance < 30.0 && sensor2Distance > 30.0) {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_PARTY_PALETTE);
+        } else if (sensor1Distance < distanceMax && sensor2Distance < distanceMax)
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        else {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+        }
     }
 
     public void setControlArmToHighBoard (){
@@ -64,10 +96,12 @@ public class ExtakeSubsystem extends FalconSubsystemBase {
      */
     public void pixelIntake(){
        pixelWheelDirection(DcMotorSimple.Direction.FORWARD);
+       detectPixel();
        telemetry.addData("Pixel Intake: ", pixelWheel.getPower());
        telemetry.addData("Pixel Intake: ", pixelWheel.getDirection());
     }
     public void pixelEject (){
+        detectPixel();
         pixelWheelDirection(DcMotorSimple.Direction.REVERSE);
         telemetry.addData("Pixel Eject: ", pixelWheel.getPower());
         telemetry.addData("Pixel Eject: ", pixelWheel.getDirection());
