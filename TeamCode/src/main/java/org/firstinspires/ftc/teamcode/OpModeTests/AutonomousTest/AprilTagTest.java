0514adecc3926eb13c8badb323daafabf6f76b03
rfouty <roth.fouty@gmail.com>
2024-01-23 19:30:42 -0800

    Added AprilTagManualTest_Op for testing DriveToAprilTagCommand
    Updated AprilTagTest for testing purposes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AprilTagTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTests/AutonomousTest/AprilTagTest.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.OpModeTests.AutonomousTest;
 
 import android.service.autofill.FieldClassification;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.ScheduleCommand;
@@ -38,6 +40,7 @@ public class AprilTagTest extends CommandOpMode {
     @Override
     public void initialize() {
         CommandScheduler.getInstance().reset();
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         MatchConfig.Alliance = alliance;
         MatchConfig.TeamPropPosition = teamPropPosition;
@@ -48,24 +51,24 @@ public class AprilTagTest extends CommandOpMode {
         DistanceSensorSubsystem distanceSensorSubsystem = new DistanceSensorSubsystem(hardwareMap, telemetry);
 
         schedule(new SequentialCommandGroup(
-                new AprilTagStartStopCommand(visionSubsystem, AprilTagStartStopCommand.State.Start),
+                //new AprilTagStartStopCommand(visionSubsystem, AprilTagStartStopCommand.State.Start),
                 //new StrafeToFindAprilTagCommand(mecanumDriveSubsystem, visionSubsystem),
-                new DriveToAprilTagCommand(visionSubsystem, mecanumDriveSubsystem),
-                new DriveForwardToObjectCommand(mecanumDriveSubsystem, distanceSensorSubsystem, GyroSubsystem.getInstance(hardwareMap, telemetry), Configuration.BACKDROP_DISTANCE),
-                new AprilTagStartStopCommand(visionSubsystem, AprilTagStartStopCommand.State.ClosePortal)
+                new DriveToAprilTagCommand(visionSubsystem, mecanumDriveSubsystem)
+                //new DriveForwardToObjectCommand(mecanumDriveSubsystem, distanceSensorSubsystem, GyroSubsystem.getInstance(hardwareMap, telemetry), Configuration.BACKDROP_DISTANCE),
+                //new AprilTagStartStopCommand(visionSubsystem, AprilTagStartStopCommand.State.ClosePortal)
                 ));
 
         register(mecanumDriveSubsystem, visionSubsystem, distanceSensorSubsystem, GyroSubsystem.getInstance(hardwareMap, telemetry));
     }
 
-    @Override
-    public void runOpMode() throws InterruptedException{
-        try {
-            super.runOpMode();
-        }
-        catch(Exception ex){
-            MatchConfig.telemetry.addLine(ex.getMessage());
-        }
-        MatchConfig.telemetry.update();
-    }
+//    @Override
+//    public void runOpMode() throws InterruptedException{
+//        try {
+//            super.runOpMode();
+//        }
+//        catch(Exception ex){
+//            MatchConfig.telemetry.addLine(ex.getMessage());
+//        }
+//        MatchConfig.telemetry.update();
+//    }
 }
